ab223a14c7c0cd178a4338cf4f9f34618783e004
Théo <theo.delas@ynov.com>
2020-09-29 17:40:13 +0200

    :sparkles: Add counter with button who increment this in body

--- user_interface/lib/main.dart
+++ user_interface/lib/main.dart
@@ -28,9 +28,9 @@ class TutorialHome extends StatelessWidget {
         ],
       ),
       // body is the majority of the screen.
-      body: Center(
-        child: Text('Hello, world!'),
-      ),
+      body: Container(
+          margin: const EdgeInsets.symmetric(vertical: 50.0),
+          child: Center(child: Counter())),
       floatingActionButton: FloatingActionButton(
         onPressed: null,
         child: MyButton(),
@@ -58,6 +58,51 @@ class MyButton extends StatelessWidget {
           child: Icon(Icons.add),
         ),
       ),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  // This class is the configuration for the state. It holds the
+  // values (in this case nothing) provided by the parent and used
+  // by the build  method of the State. Fields in a Widget
+  // subclass are always marked "final".
+
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      // This call to setState tells the Flutter framework that
+      // something has changed in this State, which causes it to rerun
+      // the build method below so that the display can reflect the
+      // updated values. If you change _counter without calling
+      // setState(), then the build method won't be called again,
+      // and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called,
+    // for instance, as done by the _increment method above.
+    // The Flutter framework has been optimized to make rerunning
+    // build methods fast, so that you can just rebuild anything that
+    // needs updating rather than having to individually change
+    // instances of widgets.
+    return Column(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: _increment,
+          child: Text('Increment'),
+        ),
+        Text('Count: $_counter'),
+      ],
     );
   }
 }
